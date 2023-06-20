afee6bff4633f015787ea175d72fcbf7c0bb7cf7
George Gianacopoulos <gpg@cs.toronto.edu>
2023-06-20 16:43:05 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- Famous Paintings
+
+famous_paintings = {}
+
+local default_path = minetest.get_modpath("famous_paintings")
+
+dofile(default_path .. "/paintings.lua")
+
---
+++ paintings.lua
@@ -0,0 +1,9 @@
+-- 1x1
+
+paintings_lib.register1x1(
+  "the_scream", 
+  "The Scream - Edvard Munch (1893)", 
+  "fp_painting_1x1_the-scream.png^fp_frame_1x1_gilded.png"
+)
+
+
