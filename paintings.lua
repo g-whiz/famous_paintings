8444bb1f273ce42cc6af39af43e80ade4afaf774
George Gianacopoulos <gpg@cs.toronto.edu>
2023-06-23 23:28:42 -0400

    Added new paintings.

--- paintings.lua
+++ paintings.lua
@@ -1,9 +1,133 @@
--- 1x1
+-- The Scream
 
 paintings_lib.register1x1(
   "the_scream", 
   "The Scream - Edvard Munch (1893)", 
-  "fp_painting_1x1_the-scream.png^fp_frame_1x1_gilded.png"
+  "fp_painting_1x1_the-scream.png^paintings_lib_frame1x1.png"
 )
 
+-- Metamorphose II
 
+-- TODO: add 4x1 model (frameless)
+
+--paintings_lib.register4x1(
+--        "metamorphose_ii_a",
+--        "Metamorphose II - M.C. Escher (1940)",
+--        "fp_painting_1x1_metamorphose-ii-a.png"
+--)
+--
+--paintings_lib.register4x1(
+--        "metamorphose_ii_b",
+--        "Metamorphose II - M.C. Escher (1940)",
+--        "fp_painting_1x1_metamorphose-ii-b.png"
+--)
+--
+--paintings_lib.register4x1(
+--        "metamorphose_ii_b",
+--        "Metamorphose II - M.C. Escher (1940)",
+--        "fp_painting_1x1_metamorphose-ii-b.png"
+--)
+--
+--paintings_lib.register4x1(
+--        "metamorphose_ii_a",
+--        "Metamorphose II - M.C. Escher (1940)",
+--        "fp_painting_1x1_metamorphose-ii-a.png"
+--)
+--
+--paintings_lib.register4x1(
+--        "metamorphose_ii_a",
+--        "Metamorphose II - M.C. Escher (1940)",
+--        "fp_painting_1x1_metamorphose-ii-a.png"
+--)
+--
+--paintings_lib.register1x1(
+--        "metamorphose_ii_f",
+--        "Metamorphose II - M.C. Escher (1940)",
+--        "fp_painting_1x1_metamorphose-ii-f.png"
+--)
+
+-- Girl with a Pearl Earring
+
+paintings_lib.register1x1(
+        "girl_with_a_pearl_earring",
+        "Girl with a Pearl Earring - Johannes Vermeer (1685)",
+        "fp_painting_1x1_girl-with-a-pearl-earring.png^paintings_lib_frame1x1.png"
+)
+
+-- The Indian Church
+
+paintings_lib.register1x2(
+        "the_indian_church",
+        "The Indian Church - Emily Carr (1929)",
+        "fp_painting_1x2_the-indian-church.png^paintings_lib_frame1x2.png"
+)
+
+-- Composition with Red, Blue, and Yellow
+
+paintings_lib.register2x2(
+        "composition_ii_in_red_blue_and_yellow",
+        "Composition with Red, Blue and Yellow - Piet Mondrian (1929)",
+        "fp_painting_2x2_composition-with-red-blue-and-yellow.png^paintings_lib_frame2x2.png"
+)
+
+-- Mona Lisa
+
+paintings_lib.register2x3(
+        "mona_lisa",
+        "Mona Lisa - Leonardo da Vinci (1506)",
+        "fp_painting_2x3_mona-lisa.png^paintings_lib_frame2x3.png"
+)
+
+paintings_lib.register3x4(
+        "american_gothic",
+        "American Gothic - Grant Wood (1930)",
+        "fp_painting_3x4_american-gothic.png^paintings_lib_frame3x4.png"
+)
+
+-- Guernica
+
+paintings_lib.register4x2(
+        "guernica",
+        "Guernica - Pablo Picasso (1937)",
+        "fp_painting_4x2_guernica.png^paintings_lib_frame4x2.png"
+)
+
+-- Water Lilies
+
+paintings_lib.register4x2(
+        "water-lilies",
+        "Water Lilies - Claude Monet (1917)",
+        "fp_painting_4x2_water-lilies.png^paintings_lib_frame4x2.png"
+)
+
+-- A Sunday on La Grande Jatte
+
+paintings_lib.register4x3(
+        "a-sunday-on-la-grande-jatte",
+        "A Sunday on La Grande Jatte - Georges Seurat (1886)",
+        "fp_painting_4x3_a-sunday-on-la-grande-jatte.png^paintings_lib_frame4x3.png"
+)
+
+-- Lake and Mountain
+
+paintings_lib.register4x3(
+        "lake-and-mountain",
+        "Lake and Mountain - Lawren Harris (1928)",
+        "fp_painting_4x3_lake-and-mountain.png^paintings_lib_frame4x3.png"
+)
+
+-- The Persistence of Memory
+
+paintings_lib.register4x3(
+        "the-persistence-of-memory",
+        "The Persistence of Memory - Salvador Dali (1931)",
+        "fp_painting_4x3_the-persistence-of-memory.png^paintings_lib_frame4x3.png"
+)
+
+-- The Starry Night
+
+paintings_lib.register4x3(
+        "the-starry-night",
+        "The Starry Night - Vincent van Gogh (1889)",
+        "fp_painting_4x3_the-starry-night.png^paintings_lib_frame4x3.png"
+)
